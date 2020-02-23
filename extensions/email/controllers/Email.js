153113b0609153fd35d75c3991936939aed64ceb
Carsten Bleek <bleek@cross-solution.de>
2020-02-23 17:23:00 +0100

    requires "to" parameter for sending mails.

--- extensions/email/controllers/Email.js
+++ extensions/email/controllers/Email.js
@@ -6,9 +6,9 @@ module.exports = {
     console.log(ctx.request.body);
     try {
       var info = await strapi.plugins.email.services.email.send({
-              to: 'bleek@cross-solution.de', // Well make this configurable via .env
-              from: 'bleek@cross-solution.de', // Well make this configurable via .env
-              replyTo: 'no-reply@me.com',
+              to: body.to,
+              from: body.from,
+              replyTo: '${body.name} <${body.email>',
               subject: 'Hello',
               text: `This is ${body.name}. Mail was sent by <${body.email}>`,
               html: body.message
